9b3cebcb1f1d3b3326978a87e2b0edc6c3716607
Jaxolotl20112023 <jaxolotl7@gmail.com>
2026-05-04 06:53:47 -1000

    lines collied when it wraps to the next line.

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ running = True
 
 class button : 
 
-    def __init__(self, screen, width, height, x, y, content):
+    def __init__(self, screen, width, height, x, y, content, fontSize):
         self.screen = screen
         self.width = width
         self.height = height
         self.x = x 
         self.y = y
         self.content = content
+        self.fontSize = fontSize
         
     def draw(self) : 
         # width = 100
@@ -31,10 +32,11 @@ class button :
         y = self.y 
         screen = self.screen
         content = self.content
+        fontSize = self.fontSize
         
         
         
-        font = py.font.SysFont("Arial", 15)
+        font = py.font.SysFont("Arial", fontSize)
         text = font.render(content, True, (0,0,0))
 
         self.button = py.rect.Rect(x,y,width,height)
@@ -58,8 +60,9 @@ class textField:
         self.x = 50
         self.y = 50
         self.wrapLength = 200
+        self.fontSize = 30
 
-        self.font = py.font.SysFont("Arial", 30)
+        self.font = py.font.SysFont("Arial", self.fontSize)
 
         self.border = py.rect.Rect(self.x,self.y,SCREEN_WIDTH-100,SCREEN_HEIGHT-100)
         py.draw.rect(self.screen, (255,255,255), self.border)
@@ -75,17 +78,25 @@ class textField:
         new_tasks = []
         self.d_buttons = []
 
+        moveDown = self.fontSize
+
         py.draw.rect(self.screen, (255,255,255), self.border)
 
         for lines in range(0,len(tasks)) : 
-            self.d_buttons.append(button(self.screen,50,25,self.x+wrapLength,self.y,"done"))
+            self.d_buttons.append(button(self.screen,50,25,self.x+wrapLength,self.y,"done",15))
             self.d_buttons[len(self.d_buttons)-1].draw()
 
             text = self.font.render(tasks[lines][0], True, (0,0,0), wraplength=wrapLength)
             self.screen.blit(text,(self.x,self.y))
 
             new_tasks.append([tasks[lines][0],self.y])
-            self.y+=30
+
+            if len(tasks[lines][0]) >= 6 :
+                moveDown = 30 * round(len(tasks[lines][0]),1) % 6
+            else : 
+                moveDown = 30 
+
+            self.y+=moveDown
 
         self.y = 50
 
@@ -98,7 +109,7 @@ screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 s_color = (196,238,255)
 screen.fill(s_color)
 
-b_addTask = button(screen, 100, 50, SCREEN_WIDTH/2, SCREEN_HEIGHT-100,"Add Task")
+b_addTask = button(screen, 100, 50, SCREEN_WIDTH-150, SCREEN_HEIGHT-100,"Add Task",30)
 b_addTask.draw()
 t_textField = textField("hello", screen)
 
@@ -132,11 +143,6 @@ while running :
                         tasks = t_textField.change(tasks)
 
                         break
-
-            
-
-            
-            
 
     b_addTask.draw()
     py.display.update()
